bf6065da46c7211b796f02adb4470f8f51fd7f1a
Francis Bulus <notchera@gmail.com>
2019-12-04 22:52:23 +0100

    removed nested folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Editor } from 'slate';
 
 function App() {
   return (
     <div className="App">
-      <Editor/>
+      <Edi/>
     </div>
   );
 }
